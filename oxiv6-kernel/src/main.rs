b712f41c88e8650ac12cab3b9d6f9062dadc388d
Claire Moore <kurennon@gmail.com>
2024-04-27 13:32:54 -0700

    Move kernel to riscv64 standalone
    Builds with a given linker module stored in the `build.rs`
    
    https://github.com/rustsbi/rustsbi-qemu/blob/main/test-kernel/build.rs
    contains the source of the build script, as I intend to run the kernel
    off of RustSBI / any SBI implementation.

--- oxiv6-kernel/src/main.rs
+++ oxiv6-kernel/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+#[no_mangle]
+#[link_section = ".text.entry"]
+unsafe extern "C" fn _start() -> ! {
+    loop {}
+}
+
+#[cfg(not(test))]
+#[panic_handler]
+fn panic_handler(_info: &core::panic::PanicInfo<'_>) -> ! {
+    loop {}
 }
