ebc4c4e6da31f9ff0f0584c2d8d497f0cb2068fb
Claire Moore <kurennon@gmail.com>
2024-04-29 20:06:58 -0700

    Enable printing to the Console using SBI
    Detects which extension is available, and uses the appropriate extension.
    
    Also provides a convenient `print` and `println` implementation.

--- oxiv6-kernel/src/main.rs
+++ oxiv6-kernel/src/main.rs
@@ -2,16 +2,32 @@
 #![no_main]
 #![feature(naked_functions, asm_const)]
 
+use core::arch::{asm, global_asm};
+use core::fmt::Write;
+use spin;
+
 const TRAPFRAME: usize = 4096;
 const STACK_SIZE: usize = 4096;
 const MAX_HART_COUNT: usize = 8;
 static mut STACK_0: [[u8; STACK_SIZE]; MAX_HART_COUNT] = [[0; STACK_SIZE]; MAX_HART_COUNT];
+static PRINT_IMPL: spin::once::Once<&'static dyn DebugPrint> = spin::once::Once::new();
+
+macro_rules! print {
+    ($($arg:tt)*) => { core::write!($crate::DebugWriter, $($arg)*).expect("Unable to write!"); }
+}
+
+macro_rules! println {
+    ($($arg:tt)*) => { core::writeln!($crate::DebugWriter, $($arg)*).expect("Unable to write!"); }
+}
+
+pub(crate) use print;
+pub(crate) use println;
 
 #[naked]
 #[no_mangle]
 #[link_section = ".text.entry"]
 unsafe extern "C" fn _start(hartid: usize, device_tree_paddr: usize) -> ! {
-    core::arch::asm!(
+    asm!(
         "la sp, {stack0}",
         "li t0, {stack_size}",
         "addi t1, a0, 1",
@@ -26,11 +42,73 @@ unsafe extern "C" fn _start(hartid: usize, device_tree_paddr: usize) -> ! {
 }
 
 #[no_mangle]
-extern "C" fn rust_main(hartid: usize, device_tree_paddr: usize) -> ! {
+extern "C" fn rust_main(_hartid: usize, _device_tree_paddr: usize) -> ! {
+    if sbi_rt::probe_extension(sbi_rt::Console).is_available() {
+        PRINT_IMPL.call_once(|| &DebugConsoleDebugPrint);
+    } else {
+        PRINT_IMPL.call_once(|| &LegacyDebugPrint);
+    }
+
+    print!("Starting up Oxvi6! ");
+    println!("Shutting down...");
+    sbi_rt::system_reset(sbi_rt::Shutdown, sbi_rt::NoReason);
     loop {}
 }
 
-core::arch::global_asm!(include_str!("trampoline.S"), TRAPFRAME = const TRAPFRAME);
+trait DebugPrint : Sync {
+    fn print_byte(&self, byte: u8) -> core::fmt::Result;
+
+    fn print_str(&self, string: &str) -> core::fmt::Result {
+        for byte in string.bytes() {
+            self.print_byte(byte)?;
+        }
+        Ok(())
+    }
+}
+
+struct LegacyDebugPrint;
+
+impl DebugPrint for LegacyDebugPrint {
+    #[allow(deprecated)]
+    fn print_byte(&self, byte: u8) -> core::fmt::Result {
+        return if sbi_rt::legacy::console_putchar(byte as usize) != 0 {
+            Err(core::fmt::Error)
+        } else {
+            Ok(())
+        }
+    }
+}
+
+struct DebugConsoleDebugPrint;
+
+impl DebugPrint for DebugConsoleDebugPrint {
+    fn print_byte(&self, byte: u8) -> core::fmt::Result {
+        if sbi_rt::console_write_byte(byte).is_ok() {
+            Ok(())
+        } else {
+            Err(core::fmt::Error)
+        }
+    }
+
+    fn print_str(&self, string: &str) -> core::fmt::Result {
+        let string_bytes = string.as_bytes();
+        if sbi_rt::console_write(sbi_rt::Physical::new(string_bytes.len(), string_bytes.as_ptr() as _, 0)).is_ok() {
+            Ok(())
+        } else {
+            Err(core::fmt::Error)
+        }
+    }
+}
+
+pub(crate) struct DebugWriter;
+
+impl Write for DebugWriter {
+    fn write_str(&mut self, string: &str) -> core::fmt::Result {
+        PRINT_IMPL.get().unwrap().print_str(string)
+    }
+}
+
+global_asm!(include_str!("trampoline.S"), TRAPFRAME = const TRAPFRAME);
 
 #[cfg(not(test))]
 #[panic_handler]
